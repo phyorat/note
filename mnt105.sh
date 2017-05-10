b74f21007246a9a142ebe88bba3b12ffcd96ca61
phyorat <phyorat@sina.com>
2017-05-10 17:26:33 +0800

    mount smb service

--- mnt105.sh
+++ mnt105.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
-mount -t cifs -o username=lhzy,password=123654,uid=1000,gid=1000 //192.168.2.105/usi_share /home/lhzy02/p_test/smb105/
+mount -t cifs -o username=lhzy,password=123654,uid=1000,gid=1000 //192.168.2.105/usi_share /home/union-si/smb105/
 
